ac1be5d0f6138bbb2086c1ea12b4ee4a5faa263b
Amdr3B4st0s <113254411+Amdr3B4st0s@users.noreply.github.com>
2022-11-26 14:49:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+function Calculadora(){
+    let Operac = prompt("Digite a operação desejada (+, -, * ou /).");
+    let Valor1 = prompt("Digite o primeiro valor.");
+    let Valor2 = prompt("Digite o segundo valor.");
+    let Result;
+    switch(Operac){
+        case '+':
+            Result = Number(Valor1) + Number(Valor2);
+            alert(`Resultado: ${Result}.`);
+        break;
+        case '-':
+            Result = Number(Valor1) - Number(Valor2);
+            alert(`Resultado: ${Result}.`);
+        break;
+        case '*':
+            Result = Number(Valor1) * Number(Valor2);
+            alert(`Resultado: ${Result}.`);
+        break;
+        case '/':
+            Result = Number(Valor1) / Number(Valor2);
+            alert(`Resultado: ${Result}.`);
+        break;
+    }
+}
+function Banco(){
+    let Saldo = prompt("Digite o saldo da conta.");
+    let Valor = prompt("Digite o valor da compra.");
+    if(Saldo >= Valor){
+        alert(`A compra foi realizada com sucesso. O seu novo saldo é de R$${Saldo-Valor}.`);
+    }
+    else{
+        alert("A compra não foi realizada. Saldo insuficiente.");
+    }
+}
+function Media(){
+    let Max = prompt("Digite quantos números deseja digitar.");
+    let Sum = 0;
+    for(let i = 0;i<Number(Max);i++){
+        let num = prompt(`Digite o número ${i}.`);
+        Sum = Number(Sum) + Number(num);
+    }
+
+    Sum = Number(Sum) / Number(Max);
+    alert(`A média dos números é igual a ${Sum}.`);
+}
+function SomIm(){
+    let Min = prompt("Digite o número mínimo.");
+    let Max = prompt("Digite o número máximo.");
+    let Sum = 0;
+    let Aux = Min;
+
+    if(Max<Min){
+        Aux = Min;
+        Min = Max;
+        Max = Aux;
+        Aux = Min;
+    }
+    
+    if(Number(Min)%2 == 0){
+        Min = Number(Min) + 1;
+    }
+
+    while(Number(Min)<=Number(Max)){
+        Sum = Number(Sum) + Number(Min);
+        Min = Number(Min) + 2;
+    }
+
+    alert(`A soma dos números ímpares entre ${Aux} e ${Max} é igual a ${Sum}.`);
+}
